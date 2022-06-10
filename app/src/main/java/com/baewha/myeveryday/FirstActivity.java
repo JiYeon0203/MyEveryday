ae39f3bf83422b6fcafe91b02d7756fcc0e54162
parkjiyeon <gkdis310313@gmail.com>
2022-06-10 21:39:07 +0900

    Initial commit

---
+++ app/src/main/java/com/baewha/myeveryday/FirstActivity.java
@@ -0,0 +1,4 @@
+package com.baewha.myeveryday;
+
+public class FirstActivity {
+}
---
+++ app/src/main/java/com/baewha/myeveryday/MainActivity.java
@@ -0,0 +1,55 @@
+package com.baewha.myeveryday;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2, btn3, btn4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("건강검진 예약 어플");
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intents = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intents);
+            }
+        });
+
+        btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenta = new Intent(getApplicationContext(), ThirdActivity.class);
+                startActivity(intenta);
+            }
+        });
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenta = new Intent(getApplicationContext(), FourActivity.class);
+                startActivity(intenta);
+            }
+        });
+    }
+}
