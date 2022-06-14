887947d593b8c885a37d580cbba5862a5ca7aeb8
parkjiyeon <gkdis310313@gmail.com>
2022-06-14 13:27:44 +0900

    06-14 update

--- app/src/main/java/com/baewha/myeveryday/SixActivity.java
+++ app/src/main/java/com/baewha/myeveryday/SixActivity.java
@@ -1,15 +1,21 @@
 package com.baewha.myeveryday;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SixActivity extends AppCompatActivity {
+
+    ImageButton btn1, btn2, btn3, btn4, btn5;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,6 +42,105 @@ public class SixActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.six);
         setTitle("오늘 하루");
+
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btn4 = findViewById(R.id.btn_4);
+        btn5 = findViewById(R.id.btn_5);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(SixActivity.this);
+                dlg.setTitle("안좋아").setMessage("무슨 안좋은 일이라도 있었나봐요.\n" +
+                        "blue 노래는 어떠세요?");
+                dlg.setPositiveButton("들을래", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(getApplicationContext(), Song1.class);
+                        startActivity(intent);
+                    }
+                });
+                dlg.setNegativeButton("안들을래", null);
+                dlg.show();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(SixActivity.this);
+                dlg.setTitle("그냥 그래").setMessage("좋지도 나쁘지도 않으신가봐요.\n" +
+                        "purple 노래는 어떠세요?");
+                dlg.setPositiveButton("들을래", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(getApplicationContext(), Song1.class);
+                        startActivity(intent);
+                    }
+                });
+                dlg.setNegativeButton("안들을래", null);
+                dlg.show();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(SixActivity.this);
+                dlg.setTitle("보통").setMessage("괜찮은거 같아 보이네요.\n" +
+                        "yellow 노래는 어떠세요?");
+                dlg.setPositiveButton("들을래", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(getApplicationContext(), Song1.class);
+                        startActivity(intent);
+                    }
+                });
+                dlg.setNegativeButton("안들을래", null);
+                dlg.show();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(SixActivity.this);
+                dlg.setTitle("나쁘지않아").setMessage("오늘 하루 잘 보냈나보네요.\n" +
+                        "green 노래는 어떠세요?");
+                dlg.setPositiveButton("들을래", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(getApplicationContext(), Song1.class);
+                        startActivity(intent);
+                    }
+                });
+                dlg.setNegativeButton("안들을래", null);
+                dlg.show();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(SixActivity.this);
+                dlg.setTitle("좋아").setMessage("행복한 하루를 보냈나보네요.\n" +
+                        "pink 노래는 어떠세요?");
+                dlg.setPositiveButton("들을래", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(getApplicationContext(), Song1.class);
+                        startActivity(intent);
+                    }
+                });
+                dlg.setNegativeButton("안들을래", null);
+                dlg.show();
+            }
+        });
+
+
+
 
     }
 }
